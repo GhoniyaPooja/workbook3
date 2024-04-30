a5eadc62a54139f17c68b82541f5eeafa14a3350
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-30 09:15:19 -0400

    update on exercises 1

--- exercises/more_functions.js
+++ exercises/more_functions.js
@@ -1,5 +1,8 @@
 
 function displayMailingLabel(name, address, city, state, zip) {
-    console.log(name, address, city, state, zip); 
+    // console.log(name, address, city, state, zip); 
+    console.log(name); 
+    console.log(address); 
+    console.log(city + ",", state, zip); 
 }
 displayMailingLabel("pooja", "Este pl", "Bloomfield", "New jersey", 12345);
