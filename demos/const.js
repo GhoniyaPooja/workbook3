f77f2198eae4245963a90a34921f017d9f6cdb8a
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-30 14:24:57 -0400

    const example

---
+++ demos/const.js
@@ -0,0 +1,8 @@
+const TAX_RATE = .0675;
+TAX_RATE = 8;
+
+//use const when getting html elements into variables
+const messageParagraph = document.getElementById("messageParagraph");
+messageParagraph = document.getElementById("anotherParagraph");
+//but it will allow you to change an elements properties
+messageParagraph.innerText = "my message";
