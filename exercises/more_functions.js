0f16902666e365bc97cbf395c22e0cd67051fd6d
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-30 09:32:32 -0400

    adding numbers

--- exercises/more_functions.js
+++ exercises/more_functions.js
@@ -8,3 +8,15 @@ function displayMailingLabel(name, address, city, state, zip) {
 }
 displayMailingLabel("pooja", "Este pl", "Bloomfield", "New jersey", 12345);
 displayMailingLabel("Anjli", "Estepl 212", "Bloomfield", "New york", 12654);
+
+function numbersForSomething(num1, num2) {
+    var result = num1 + num2;
+    console.log(num1, "+", num2, "=", result);
+    console.log();
+}
+
+var x = 20;
+var y = 30;
+
+numbersForSomething(x, y);
+
