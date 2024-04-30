bffa8fe3aef32a663693da0860ebbe75d0737b88
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-30 08:50:39 -0400

    update

--- demos/add.js
+++ demos/add.js
@@ -1,9 +1,24 @@
+//number 1 and number 2 are function parameters
 function add(number1, number2) {
-    var result = number1 + number2;
-    console.log(result);
-  }
-  
-  var x = 67;
-  var y = 34;
-  
-  add(x, y);
+  var result = number1 + number2;
+  console.log(result);
+}
+
+var x = 67;
+var y = 34;
+//x and y are arguments being passed to the function
+add(x, y);
+
+add(1,1)
+add(2,2)
+add(3,3)
+
+
+
+// function bake(ingredient1, ingredient2, ingredient3) {
+//     if(ingredient1 =="apples"){
+//         console.log("apple pie");
+//     }
+// }
+
+// bake("apples", "cinnamon", "sugar")
