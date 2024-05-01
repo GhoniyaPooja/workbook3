55b6c415d71215128c4666ff851c7d78c2d47c30
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-01 14:43:24 -0400

    more date conversion

---
+++ exercises/dates/more_date_conversion.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let birthday = new Date(1999, 11, 12);
+console.log(birthday.toLocaleString());
+
+let momsBirthday = new Date(1970, 11, 10);
+console.log(momsBirthday.toLocaleString());
+
+let dadsBirthday = new Date(1973, 11, 14);
+console.log(dadsBirthday.toLocaleString());
+
