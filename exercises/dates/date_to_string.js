d48468ceb62a2c90ae22df42fe32da9fb9b53990
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-01 13:57:32 -0400

    exercise of date

---
+++ exercises/dates/date_to_string.js
@@ -0,0 +1,16 @@
+"use strick";
+//Exercise
+
+let d = new Date();
+console.log(d.toString());
+
+console.log(d.toDateString());
+
+console.log(d.toUTCString());
+
+console.log(d.toLocaleString());
+
+
+
+
+
